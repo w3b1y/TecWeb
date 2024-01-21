89c5c5888394a95a0d195e9bd5e577ed8dda67ee
elena_chilese <elena.chilese@studenti.unipd.it>
2024-01-21 20:13:41 +0100

    link php

--- funzioni.php
+++ funzioni.php
@@ -1,11 +1,8 @@
 <?php
 function clearInput($value){
         $value = trim($value);
-        echo "$value";
         $value = strip_tags($value);
-        echo "$value";
         $value = htmlentities($value);
-        echo "$value";
         return $value;
     }
 
