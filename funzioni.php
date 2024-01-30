d60c4b41a0ac3086b2fd1be3853cc5d5fa7f81a6
Riccardo Fabbian <fabb.riccardo@gmail.com>
2024-01-30 09:08:25 +0100

    Corrected route problem

--- funzioni.php
+++ funzioni.php
@@ -7,18 +7,43 @@ function clearInput($value){
     }
 
 function getDateInterval(string $timeInterval) {
+    $timeInterval = str_replace('PT-', 'PT', $timeInterval);
     list($hours, $minutes, $seconds) = explode(':', $timeInterval);
     return new DateInterval("PT{$hours}H{$minutes}M");
 }
 
 function date2txt(DateTime $date){
     $txt="";
-    if($date->format('H')!='0'){
-        $txt.= $date->format('H')." ore ";
-    }
-    if($date->format('m')!='0'){
-        $txt.= $date->format('m')." minuti ";
-    }
+    if($date->format('H')=='1') $txt.= $date->format('H')." ora ";
+    else if($date->format('H')!='0') $txt.= $date->format('H')." ore ";
+
+    if($date->format('i')=='1') $txt.= $date->format('i')." minuto ";
+    else if($date->format('i')!='0') $txt.= $date->format('i')." minuti ";
     return $txt;
+}
+
+function sortDate($array, $new) {
+    $length = count($array);
+
+    if ($length === 0) {
+        return 0;
+    }
+
+    $low = 0;
+    $high = $length - 1;
+
+    while ($low <= $high) {
+        $mid = (int)(($low + $high) / 2);
+        $compare = $new <=> $array[$mid];
+
+        if ($compare < 0) {
+            $high = $mid - 1;
+        } elseif ($compare > 0) {
+            $low = $mid + 1;
+        } else {
+            return $mid;
+        }
+    }
+    return $low;
 }
 ?>
--- tickets.php
+++ tickets.php
@@ -15,6 +15,9 @@ if (!isset($_SESSION['user']) && !isset($_SESSION['admin'])) $fileHTML = str_rep
 if (isset($_SESSION['user'])) $fileHTML = str_replace("<navbar/>", '<a class="nav__link" href="userpage.php" lang="en-US">Account</a>', $fileHTML);
 if (isset($_SESSION['admin'])) $fileHTML = str_replace("<navbar/>", '<a class="nav__link" href="adminpage.php" lang="en-US">Account</a>', $fileHTML);
 
+$departure_array = array();
+$ticket_array = array();
+
 if (isset($_POST['schedule'])) {
     $_SESSION['ricerca']['schedule'] = $_POST['schedule'];
     $_SESSION['ricerca']['class'] = $_POST['class'];
@@ -39,9 +42,10 @@ if(isset($_SESSION['ricerca'])){
     $dateTime = new DateTime($dataOra);
 
     $qResult_route = $connessione->getDataArray("select start.route_id from route_station as start, route_station as end 
-        where start.station_id='$stazionePartenza' and end.station_id='$stazioneArrivo' and start.route_id=end.route_id");
+        where start.station_id='$stazionePartenza' and end.station_id='$stazioneArrivo' and start.route_id=end.route_id and 
+        start.order_number <= end.order_number");
 
-    $ticket = "";
+    $tickets = "";
 
     if (empty($qResult_route)) {
         $fileHTML = str_replace("<biglietti/>", "<p class=\"container__heading\">Attualmente Iberu Trasporti non offre la tratta da lei cercata,
@@ -57,12 +61,19 @@ if(isset($_SESSION['ricerca'])){
         $qResult_price = $connessione->getDataArray("select start.price - end.price from route_station as start join route_station as end on 
             start.route_id = end.route_id where start.station_id = '$stazionePartenza' and end.station_id = '$stazioneArrivo' and start.route_id = $ris");
         $qResult_price[0] = number_format(($qResult_price[0] - $qResult_price[0] * $discount / 100) * $nPasseggeri, 2);
+        
         $qResult_time = $connessione->getDataArray("select route_station.duration from route_station where route_station.route_id=$ris 
             and route_station.station_id='$stazionePartenza'");
-        $departure_time_route = $dateTime->sub(getDateInterval($qResult_time[0]));
+        $dateInterval = getDateInterval($qResult_time[0]);
+        $departure_time_route = clone $dateTime;
+        $departure_time_route->sub($dateInterval);
+
+        if ($departure_time_route->format('d') < $dateTime->format('d')) $departure_time_route->setTime(0, 0);
+
         $qResult_duration = $connessione->getDataArray("select timediff(end.duration, start.duration) as time_difference
-            from route_station as start join route_station as end on start.route_id = end.route_id
+            from route_station as start join route_station as end on start.route_id = end.route_id and start.route_id = $ris
             where start.station_id = '$stazionePartenza' and end.station_id = '$stazioneArrivo'");
+
         $qResult_departure = $connessione->getDataArray("select route_schedule.id, route_schedule.departure_time from route_schedule where route_schedule.route_id=$ris 
             and route_schedule.departure_time >= '".$departure_time_route->format('H:i')."'");
 
@@ -74,8 +85,9 @@ if(isset($_SESSION['ricerca'])){
 
         $total_departures = count($qResult_departure);
         $counter_departures = 0;
+
         foreach ($qResult_departure as $dt) {
-            $departure_time_station = (new DateTime($dt['departure_time']))->add(getDateInterval($qResult_time[0]));
+            $departure_time_station = (new DateTime($dt['departure_time']))->add($dateInterval);
             $arrive_time_station = clone $departure_time_station; 
             $arrive_time_station->add(getDateInterval($qResult_duration[0]));
             $train_id = $connessione->getDataArray("select route_schedule.train_id from route_schedule where route_schedule.route_id=$ris 
@@ -86,6 +98,7 @@ if(isset($_SESSION['ricerca'])){
             $total_stations = count($route_stations);
             $counter_stations = 0;
             $previous_time = null;
+
             foreach ($route_stations as $rs) {
                 $time = new DateTime($dt['departure_time']);
                 $time->add(getDateInterval($rs["duration"]));
@@ -98,7 +111,7 @@ if(isset($_SESSION['ricerca'])){
                     $duration->sub(getDateInterval($previous_time));
                     $ladder = $ladder . '<dt class="route__term--vertical vline">Durata</dt>
                     <dd class="route__data--vertical"><time datetime="'.
-                    $duration->format('H:i').'">'.$duration->format('H:i').'</time></dd>
+                    $duration->format('H:i').'">'.date2txt($duration).'</time></dd>
                     <dt class="route__term--vertical">'.$rs["station_id"].'</dt>
                     <dd class="route__data--vertical"><time datetime="'.$time->format('H:i').'">'.$time->format('H:i').'</time></dd>';
                 }
@@ -106,7 +119,11 @@ if(isset($_SESSION['ricerca'])){
                 $previous_time = $rs["duration"];
             }
 
-            $ticket .= '<article id="'.$counter.'" class="ticket js-ticket">
+            $pos = sortDate($departure_array, $departure_time_station->format('d/m/y H:i'));
+            array_splice($departure_array, $pos, 0, $departure_time_station->format('d/m/y H:i'));
+            
+            
+            $ticket = '<article id="<ticket_id/>" class="ticket js-ticket">
                         <dl class="ticket__route--horizontal">
                         <dt class="route__term--horizontal">'.$stazionePartenza.'</dt>
                         <dd class="route__data--horizontal"><time class="js-departure_time" datetime="'.
@@ -118,14 +135,14 @@ if(isset($_SESSION['ricerca'])){
                         <dd class="route__data--horizontal"><time class="js-arrival_time" datetime="'.
                         $arrive_time_station->format('H:i').'">'.$arrive_time_station->format('H:i').'</time></dd>
                         </dl>
-                        <a href="#'.(($counter_departures==$total_departures - 1 && $counter_routes==$total_routes - 1) ? 0 : ($counter + 1)).
-                        '" class="visually-hidden">Prossimo biglietto</a>
+                        <a href="#<next_ticket_id/>" class="visually-hidden">Prossimo biglietto</a>
                         <div class="ticket__body js-ticket__body ticket__body--reduced">
                         <dl class="ticket__route--vertical">'.
                         $ladder
                         .'</dl>
                         <div class="ticket__description">
-                            <p class="ticket__content">Data: '.$dateTime->format("d/m/y").'</p>
+                            <p class="ticket__content">Data: '.(($departure_time_station->format('H:i') >= $dateTime->format('H:i')) ?
+                                $dateTime->format("d/m/y") : ((clone $dateTime)->modify('+1 day'))->format("d/m/y")).'</p>
                             <p class="ticket__content">Identificativo treno: '.$train_id[0].'</p>
                             <p>Scegli la classe:</p>
                             <button class="ticket__class js-first__class ticket__class--selected">Prima classe</button>
@@ -139,15 +156,23 @@ if(isset($_SESSION['ricerca'])){
                         .number_format($qResult_price[0] * 0.8, 2).'">€'.$qResult_price[0].'</button>
                         </div>
                     </article>';
+            array_splice($ticket_array, $pos, 0, $ticket);
             $counter++;
             $counter_departures++;
         }
         $counter_routes++;
     }
+    $pos_counter = 0;
+    foreach($ticket_array as &$t) {
+        $t = str_replace("<ticket_id/>", $pos_counter, $t);
+        $t = str_replace("<next_ticket_id/>", ($pos_counter < count($ticket_array) - 1 ? $pos_counter + 1 : 0), $t);
+        $pos_counter++;
+    }
+    $tickets = implode($ticket_array);
 
 } else {
     echo "Dati non disponibili.";
 }
-$fileHTML = str_replace("<biglietti/>", $ticket, $fileHTML);
+$fileHTML = str_replace("<biglietti/>", $tickets, $fileHTML);
 echo $fileHTML;
 ?>
